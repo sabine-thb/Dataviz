37cea39dcf3fd1e138ecdda9591eb7a06d1f7e1f
sabine-thb <sabine.thibout@edu.univ-eiffel.fr>
2023-10-19 16:20:11 +0200

    chartJS

---
+++ data.js
@@ -0,0 +1,81 @@
+export const dataMonde = {
+    facteur: [
+        { cause: "Fléchissement économique", count: 0.62 },
+        { cause: "Fléchissement économique + catastrophes climatiques", count: 1.2 },
+        { cause: "Fléchissement économique + conflits", count: 2.23 },
+        { cause: "Fléchissement économique + catastrophes climatiques + conflits", count: 3.25 },
+        { cause: "Pas de fléchissement économique", count: 0.5 }
+    ],
+    evolution: [
+        { year: 2005, value: 12.4 },
+        { year: 2010, value: 9.2 },
+        { year: 2015, value: 8.3 },
+        { year: 2016, value: 8.3 },
+        { year: 2017, value: 8.1 },
+        { year: 2018, value: 8.3 },
+        { year: 2019, value: 8.4 },
+        { year: 2020, value: 9.9 }
+    ]
+}; 
+
+export const datAfrique = {
+    facteur: [
+        { cause: "Fléchissement économique", count: 0.62 },
+        { cause: "Fléchissement économique + catastrophes climatiques", count: 1.2 },
+        { cause: "Fléchissement économique + conflits", count: 2.23 },
+        { cause: "Fléchissement économique + catastrophes climatiques + conflits", count: 3.25 },
+        { cause: "Pas de fléchissement économique", count: 0.5 }
+    ],
+    evolution: [
+        { year: 2005, value: 12.4 },
+        { year: 2010, value: 9.2 },
+        { year: 2015, value: 8.3 },
+        { year: 2016, value: 8.3 },
+        { year: 2017, value: 8.1 },
+        { year: 2018, value: 8.3 },
+        { year: 2019, value: 8.4 },
+        { year: 2020, value: 9.9 }
+    ]
+}; 
+
+export const datAsie = {
+    facteur: [
+        { cause: "Fléchissement économique", count: 0.62 },
+        { cause: "Fléchissement économique + catastrophes climatiques", count: 1.2 },
+        { cause: "Fléchissement économique + conflits", count: 2.23 },
+        { cause: "Fléchissement économique + catastrophes climatiques + conflits", count: 3.25 },
+        { cause: "Pas de fléchissement économique", count: 0.5 }
+    ],
+    evolution: [
+        { year: 2005, value: 12.4 },
+        { year: 2010, value: 9.2 },
+        { year: 2015, value: 8.3 },
+        { year: 2016, value: 8.3 },
+        { year: 2017, value: 8.1 },
+        { year: 2018, value: 8.3 },
+        { year: 2019, value: 8.4 },
+        { year: 2020, value: 9.9 }
+    ]
+}; 
+
+export const datAmlat = {
+    facteur: [
+        { cause: "Fléchissement économique", count: 0.62 },
+        { cause: "Fléchissement économique + catastrophes climatiques", count: 1.2 },
+        { cause: "Fléchissement économique + conflits", count: 2.23 },
+        { cause: "Fléchissement économique + catastrophes climatiques + conflits", count: 3.25 },
+        { cause: "Pas de fléchissement économique", count: 0.5 }
+    ],
+    evolution: [
+        { year: 2005, value: 12.4 },
+        { year: 2010, value: 9.2 },
+        { year: 2015, value: 8.3 },
+        { year: 2016, value: 8.3 },
+        { year: 2017, value: 8.1 },
+        { year: 2018, value: 8.3 },
+        { year: 2019, value: 8.4 },
+        { year: 2020, value: 9.9 }
+    ]
+}; 
+
+
---
+++ myChart.js
@@ -0,0 +1,150 @@
+
+import {dataMonde, datAfrique, datAsie, datAmlat} from './data.js'
+
+  new Chart(
+    document.getElementById('myChart'),
+    {
+      type: 'bar',
+      data: {
+        labels: dataMonde.facteur.map(row => row.cause),
+        datasets: [
+          {
+            label: 'Evolution de la population en sous nutrition',
+            data: dataMonde.facteur.map(row => row.count),
+            backgroundColor: [
+                '#890D3A',
+                '#B4BC96',
+                '#D0B48F',
+                '#A1958D',
+                '#4B493D',
+              ],
+              barPercentage: 0.7, // Modifier la largeur des labels "cause"
+              borderRadius: 10,
+            
+          }
+          
+        ]
+        
+      },
+      options: {
+        indexAxis: 'y',
+        scales: {
+            x: {
+                display: true,
+                min: 0,
+                max: 6,
+                grid: {
+                    color: 'white' // Définir la couleur de la grille en blanc
+                },
+                ticks: {
+                    stepSize: 1,
+                    color: 'black', // Définir la couleur des labels en blanc
+                },
+            },
+            y: {
+                grid: {
+                    color: 'white' // Définir la couleur de la grille en blanc
+                },
+                ticks: {
+                    stepSize: 1,
+                    color: 'black', // Définir la couleur des labels en 
+
+                },
+            }
+            // maintainAspectRatio:true,
+
+        },
+
+        plugins: {
+        legend: {
+          display: false // Pour masquer la légende
+        }
+        },
+      
+    }
+
+      
+    
+    }
+  );
+
+  //Je mets en place le deuxième graphique 
+
+  new Chart(
+    document.getElementById('myChart2'),
+    {
+      type: 'line',
+      data:{
+        labels: dataMonde.evolution.map(row => row.year),
+        datasets: [
+          {
+            label: 'Evolution de la population en sous nutrition',
+            data: dataMonde.evolution.map(row => row.value),
+            pointRadius: 8, // Réglez la taille des points à 8 pixels
+            pointBackgroundColor: '#890D3A',
+            pointBorderColor: 'transparent',
+            borderColor:'black',
+            borderWidth:1.8,         
+          }
+          
+        ]
+        
+      },
+
+      options:{
+        scales: {
+          x: {        
+              grid: {
+                  color: 'white' // Définir la couleur de la grille en blanc
+              },
+              ticks: {
+                  color: 'black', // Définir la couleur des labels en blanc
+                  
+              },
+          },
+          y: {
+             display: true,
+             min: 0,
+             max: 25,
+             padding:80,
+              grid: {
+                  color: 'white', // Définir la couleur de la grille en blanc
+              },
+              ticks: {
+                  stepSize: 5,
+                  padding: 10,
+                  color: 'black' // Définir la couleur des labels en 
+
+              },
+              
+          }
+        },
+        plugins: {
+          legend: {
+            display: false // Pour masquer la légende
+            }
+        }
+          
+          // maintainAspectRatio:true,
+
+      }
+      
+
+
+    },
+
+    
+    );
+
+
+  //   function resizeChart2(newWidth, newHeight) {
+  //     canvas.width = newWidth;
+  //     canvas.height = newHeight;
+  //     myChart2.resize(); // Actualisez le graphique
+  // }
+  
+  // // Exemple d'appel de la fonction pour redimensionner le graphique
+  // resizeChart2(400, 800);
+
+
+
