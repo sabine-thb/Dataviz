4b08902cf78763da536e44143a1c3a7c447fe2ad
rihabzz <124267185+rihabzz@users.noreply.github.com>
2023-10-25 16:32:19 +0200

    25/10

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ function scrollToSection() {
     const section = document.querySelector(".deux");
     section.scrollIntoView({ behavior: "smooth" });
 }
+function scrollToSection() {
+    const section3 = document.getElementById('section3');
+    section3.scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  
+
+// // Ouvrir le popup en cliquant sur "Mentions légales"
+// document.querySelector(".open-popup").addEventListener("click", function() {
+//     document.querySelector(".popup-background").style.display = "flex";
+// });
+
+document.querySelector("#closePopup").addEventListener("click", function() {
+    document.querySelector(".popup-background").style.display = "none";
+});
+
+// Fermer le popup en cliquant sur la croix
+document.querySelector(".close-popup").addEventListener("click", function() {
+    document.querySelector(".popup-background").style.display = "none";
+});
