42ded79bad13fedd2b5a720ab15bdd817273069c
sabine-thb <sabine.thibout@edu.univ-eiffel.fr>
2023-11-09 10:26:42 +0100

    popupQuiMarchee

--- script.js
+++ script.js
@@ -9,22 +9,26 @@ function scrollToSection3() {
 }
 
 // On met en place l'ouverture des popups
-var lienPopup = document.querySelectorAll(".lien");
-var popup = document.querySelector(".popup");
+var lienPopup = document.querySelectorAll(".lienPopup");
 
 lienPopup.forEach(function (element) {
     element.addEventListener('click', function() {
+        var popupId = element.dataset.popup; // On obtient l'ID du popup à partir de l'attribut de données
+        var popup = document.querySelector("#" + popupId); // On sélectionne le popup correspondant
         popup.classList.add('popup-visible');
         popup.classList.remove('popup-invisible');
     });
 });  
-
 
 //On met en place la fermeture des popups
 var cancel =document.querySelectorAll(".cancel");
 
 cancel.forEach(function (element) {
     element.addEventListener('click', function() {
+        var popupElement = element.closest('.popup');
+        // Ciblez l'élément avec l'ID correspondant
+        var popupId = popupElement.id;
+        var popup = document.querySelector("#" + popupId); // On sélectionne le popup correspondant
         popup.classList.add('popup-invisible');
         popup.classList.remove('popup-visible');
     });
